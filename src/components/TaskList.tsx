a681ff82b37c8d75898fb03f82ce49cb22af358e
Daniel Araújo <daniel@integrarq.com.br>
2022-01-19 23:20:19 -0300

    First Ignite Challenge finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return; // não permite criar uma task vazia pq se o valor for false ela ja da retorno não executando o resto do código.
+    
+    const newTask ={
+      id: Math.random(), // pra gerar um id aleatório, porém é apenas um método pra testar , não recomendado o uso 
+      title: newTaskTitle,
+      isComplete: false // pra não adicionar uma task que ja esteja completa
+
+    }
+    setTasks(oldState =>[...oldState, newTask]) // callback que pega as informações anteriores e adiciona uma nova
+    setNewTaskTitle('') // Pra resetar o estado assim que for usado 
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggleTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete //traz todos valores de task e altera a propriedade
+    } : task); // retorna os valores que ja estavam
+    setTasks(toggleTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const removeTasks = tasks.filter(task => task.id !== id ) // retorna as tasks diferente do id 
+    setTasks(removeTasks)
   }
 
   return (
